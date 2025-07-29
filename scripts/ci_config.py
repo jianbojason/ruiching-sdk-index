7a6b9ca7307f2ddf17b76c97e4cbc0e4895c086f
jiangjianbo <jiangjianbo@rt-thread.com>
2025-07-29 15:35:20 +0800

    更新index server地址

--- scripts/ci_config.py
+++ scripts/ci_config.py
@@ -1,3 +1,1 @@
-
-# INDEX_SERVER_URL="https://api.rt-thread.org/studio/indexserver"
-INDEX_SERVER_URL="http://43.139.44.192:9195/indexserver"
+INDEX_SERVER_URL="https://api.rt-thread.org/studio/indexserver"
